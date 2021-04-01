9d6b62385d7d30537e59489da9bde6a825a3d53a
Cockermann <cockermann@Cockermanns-MacBook-Pro.local>
2021-04-01 08:35:56 -0700

    Initial commit

---
+++ MobiDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobiDev
+//
+//  Created by Cockermann on 01.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
